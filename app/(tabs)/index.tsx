75f672a84a135360405a16cdbc9fa50488c7fb5e
Matheus171701 <124268977+Matheus171701@users.noreply.github.com>
2024-05-28 12:13:28 +0000

    second commit

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -3,7 +3,7 @@ import { Image, StyleSheet, Platform } from 'react-native';
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView'; 
+import { ThemedView } from '@/components/ThemedView';
 
 export default function HomeScreen() {
   return (
@@ -11,58 +11,59 @@ export default function HomeScreen() {
       headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
       headerImage={
         <Image
-          source={require('@/assets/images/partial-react-logo.png')}
+          source={require('@/assets/images/logo-paralax.png')}
           style={styles.reactLogo}
         />
       }>
-      <ThemedView style={styles.titleContainer}>
-        <ThemedText type="title">Welcome!</ThemedText>
+      <ThemedView style={styles.tituloContainer}>
+        <ThemedText type="title">Bem vindo ao Ghost!</ThemedText>
         <HelloWave />
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+      <ThemedView style={styles.tituloContainer}>
+        <ThemedText type="subtitle">Sobre o app:</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.textoContainer}>
         <ThemedText>
-          Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
-          Press{' '}
-          <ThemedText type="defaultSemiBold">
-            {Platform.select({ ios: 'cmd + d', android: 'cmd + m' })}
-          </ThemedText>{' '}
-          to open developer tools.
+          <ThemedText type="defaultSemiBold">O app Ghost é um projeto com o objetivo de detectar ameaças ao seu despositivo, descobrir vírus, emails nocivo, spam,
+            bloquear ligações desconhecidas caso desejado pelo dono do dispositivo, e muito mais serviços que presam pela segurnaça do aparelho.</ThemedText>
         </ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 2: Explore</ThemedText>
+      <ThemedView style={styles.tituloContainer}>
+        <ThemedText type="subtitle">Sobre mim:</ThemedText>
+      </ThemedView>
+      <ThemedView style={styles.textoContainer}>
         <ThemedText>
-          Tap the Explore tab to learn more about what's included in this starter app.
+          Sou Matheus Henrique, aluno da ETEC Miltton Gazzetti de Presidente Vensceslau.
+          Professor: Graziani Zanfolin.
+          Meu número de telefone é: 18 98161-0390
         </ThemedText>
       </ThemedView>
-      <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 3: Get a fresh start</ThemedText>
+      <ThemedView style={styles.tituloContainer}>
+        <ThemedText type="subtitle">Próxima página:</ThemedText>
+        </ThemedView>
+        <ThemedView style={styles.textoContainer}>
         <ThemedText>
-          When you're ready, run{' '}
-          <ThemedText type="defaultSemiBold">npm run reset-project</ThemedText> to get a fresh{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> directory. This will move the current{' '}
-          <ThemedText type="defaultSemiBold">app</ThemedText> to{' '}
-          <ThemedText type="defaultSemiBold">app-example</ThemedText>.
+          Depois de ter lido esta página, vá para a págian das redes sociais, onde todas as que eu estou presente estarão lá.
         </ThemedText>
       </ThemedView>
     </ParallaxScrollView>
+
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
+  tituloContainer: {
     flexDirection: 'row',
     alignItems: 'center',
     gap: 8,
   },
-  stepContainer: {
+  textoContainer: {
     gap: 8,
     marginBottom: 8,
   },
   reactLogo: {
-    height: 178,
-    width: 290,
+    height: 300,
+    width: 400,
     bottom: 0,
     left: 0,
     position: 'absolute',
--- components/HelloWave.tsx
+++ components/HelloWave.tsx
@@ -23,7 +23,7 @@ export function HelloWave() {
 
   return (
     <Animated.View style={animatedStyle}>
-      <ThemedText style={styles.text}>👋</ThemedText>
+      <ThemedText style={styles.text}>👻</ThemedText>
     </Animated.View>
   );
 }
